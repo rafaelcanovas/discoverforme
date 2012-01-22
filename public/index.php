7825b478dc6ee8cd2bb6af7acea1e7922539b7ad
Rafael Canovas <rafaelcanovas@shortmail.me>
2012-01-22 16:46:16 -0200

    Moved document root to public/

---
+++ public/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	
+	<title>discoverfor.me</title>
+	<meta name="author" content="Rafael Canovas">
+	<meta name="description" content="Find out new influences, amplify your horizons.">
+	<meta name="keywords" content="music, discover, new, sound, audio, similar">
+	
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	
+	<link rel="shortcut icon" href="/favicon.ico">	
+	<link rel="stylesheet" href="/css/style.css?v=2">
+	<!--[if lt IE 9]>
+	<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	<script src="/js/dojo.js"></script>
+	<script src="/js/main.js"></script>
+</head>
+<?php flush(); ?>
+<body>
+	<input id="search" type="search" value="" placeholder="Don't touch this." autofocus required>
+	<div id="author">
+		<a href="mailto://rafaelcanovas@shortmail.me">
+			<img src="/images/rafaelcanovas.png" alt="&copy; 2012 Rafael Canovas" title="&copy; 2012 Rafael Canovas">
+		</a>
+	</div>
+</body>
+</html>
