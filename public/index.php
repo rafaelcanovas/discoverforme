ba8b8ae5598899c1fe9cf4284ff6ed61c1a98e37
Rafael Canovas <rafaelcanovas@shortmail.me>
2012-01-23 21:59:19 -0200

    Experimenting a new top bar

--- public/index.php
+++ public/index.php
@@ -4,15 +4,15 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	
-	<title>discoverfor.me</title>
+	<title>Audiorizon - Amplify yours</title>
 	<meta name="author" content="Rafael Canovas">
 	<meta name="description" content="Find out new influences, amplify your horizons.">
-	<meta name="keywords" content="music, discover, new, sound, audio, similar">
+	<meta name="keywords" content="music, discover, new, sound, audio, similar, albums, tracks, artists, horizon">
 	
 	<meta name="viewport" content="width=device-width,initial-scale=1">
 	
 	<link rel="shortcut icon" href="/favicon.ico">	
-	<link rel="stylesheet" href="/css/style.css?v=9">
+	<link rel="stylesheet" href="/css/style.css?v=10">
 	<!--[if lt IE 9]>
 	<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
@@ -21,7 +21,9 @@
 </head>
 <?php flush(); ?>
 <body>
-	<input id="search" type="search" value="" placeholder="Don't touch this." autofocus required>
+	<header>
+		<input id="search" type="search" value="" placeholder="Don't touch this." autofocus required>
+	</head>
 	<div id="author">
 		<a href="mailto://rafaelcanovas@shortmail.me">
 			<img src="/images/rafaelcanovas.png" alt="&copy; 2012 Rafael Canovas" title="&copy; 2012 Rafael Canovas">
