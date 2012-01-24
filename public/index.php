7f3fbd1004a0b56d2d14bc7742a81127ff00ccb9
Rafael Canovas <rafaelcanovas@shortmail.me>
2012-01-23 22:30:19 -0200

    Forgot to version it.

--- public/index.php
+++ public/index.php
@@ -17,7 +17,7 @@
 	<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
 	<script src="/js/dojo.js"></script>
-	<script src="/js/main.js"></script>
+	<script src="/js/main.js?v=2"></script>
 </head>
 <?php flush(); ?>
 <body>
