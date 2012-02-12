ff8837011a30d631f137e029bf63d8a2de5e9754
Rafael Canovas <rafaelcanovas@shortmail.me>
2012-02-12 03:59:44 -0200

    Add some CSS improvements, and a new background image. Fixed project title, description and add a new favicon

--- public/index.php
+++ public/index.php
@@ -4,15 +4,15 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	
-	<title>Audiorizon - Amplify yours</title>
+	<title>discoverfor.me ― Amplify your repertoire</title>
 	<meta name="author" content="Rafael Canovas">
-	<meta name="description" content="Find out new influences, amplify your horizons.">
-	<meta name="keywords" content="music, discover, new, sound, audio, similar, albums, tracks, artists, horizon">
+	<meta name="description" content="Find out new influences, amplify your repertoire.">
+	<meta name="keywords" content="music, discover, new, sound, audio, similar, albums, tracks, artists, repertoire">
 	
 	<meta name="viewport" content="width=device-width,initial-scale=1">
 	
 	<link rel="shortcut icon" href="/favicon.ico">	
-	<link rel="stylesheet" href="/css/style.css?v=14">
+	<link rel="stylesheet" href="/css/style.css?v=15">
 	<!--[if lt IE 9]>
 	<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
 	<![endif]-->
@@ -22,10 +22,10 @@
 <?php flush(); ?>
 <body>
 	<header>
-		<input id="search" type="search" value="" placeholder="Don't touch this." autofocus required>
+		<input id="search" type="search" value="" placeholder="C'mon, try an artist!" autofocus required>
 	</header>
 	<div id="author">
-		<a href="mailto://rafaelcanovas@shortmail.me">
+		<a href="mailto:rafaelcanovas@shortmail.me">
 			<img src="/images/rafaelcanovas.png" alt="&copy; 2012 Rafael Canovas" title="&copy; 2012 Rafael Canovas">
 		</a>
 	</div>
