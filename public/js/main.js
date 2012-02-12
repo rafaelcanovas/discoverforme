dad8f72c8e5e24cb30c20340b12c7eb317ab5b91
Rafael Canovas <rafaelcanovas@shortmail.me>
2012-02-12 05:29:14 -0200

    Tweaked a lot main.js with fixed and improvements, planning to object-orient all the code

--- public/js/main.js
+++ public/js/main.js
@@ -6,8 +6,23 @@ dojo.ready(function(){
 		dojo.toggleClass(searchInput, 'loading');
 	}
 	
-	dojo.connect(searchInput, 'onchange', null, function(e){
+	function cancelRequest(msg)
+	{
+		if(msg) {
+			searchInput.value = msg;
+		}
+		
+		searchInput.focus();
+		searchInput.select();
+		// Cancel loading
 		toggleLoading();
+	}	
+	
+	dojo.connect(searchInput, 'onchange', null,
+	function(e) {
+		// Begin loading
+		toggleLoading();
+		
 		dojo.xhrGet({
 			url: 'http://ws.audioscrobbler.com/2.0/',
 			handleAs: 'json',
@@ -15,42 +30,65 @@ dojo.ready(function(){
 				method: 'artist.getsimilar',
 				artist: e.target.value,
 				api_key: 'eb6dc9b732732664bb179c33cb94ab36',
-				autocorrect: 1,
+				autocorrect: true,
 				format: 'json'
 			},
 			load: function(data) 
 			{
-				var similarArtists = data['similarartists'];
-				var queriedArtist = (similarArtists ? similarArtists['@attr'] : false);
-				if(!similarArtists || !queriedArtist) {
-					searchInput.value = 'Who is this?';
-					searchInput.focus();
-					toggleLoading();
+				if(data.error) {
+					var errorMessage = 'Fatal error, call the ambulance!';
+					
+					if(data.error == 6) {
+						// An unknown artist was inputted
+						errorMessage = 'Who?';
+					}
+					
+					cancelRequest(errorMessage);
 					return;
 				}
 
-				dojo.query('.thumbnail').forEach(dojo.destroy);			
-				searchInput.value = queriedArtist.artist;
+				// Clear the screen if other artist was queried previously
+				dojo.query('.thumbnail').forEach(dojo.destroy);
+				
+				var similarArtists = data.similarartists;
+				
+				// Show the autocorrected input value
+				searchInput.value = similarArtists['@attr'].artist;
 			
 				for(var i in similarArtists['artist']) {
 					var similar = similarArtists['artist'][i];
 					
-					var thumbnail = dojo.create('div', {class:'thumbnail'});
-					dojo.setStyle(thumbnail, {backgroundImage: "url('"+similar.image[3]['#text']+"')"});
+					var thumbnailImg = new Image();
+					thumbnailImg.src = similar.image[3]['#text'];
 					
-					var anchor = dojo.create('a', {
+					var thumbnail = dojo.create('div', { class:'thumbnail' });
+					dojo.setStyle(thumbnail, {
+						backgroundImage: 'url(\''+thumbnailImg.src+'\')',
+						opacity: 0
+					});
+					
+					var info = dojo.create('a', {
 						class: 'info',
 						target: '_blank',
 						href: 'http://'+similar.url,
 						innerHTML: similar.name	
 					});
 					
-					dojo.place(anchor, thumbnail);
+					dojo.place(info, thumbnail);
 					dojo.place(thumbnail, dojo.body());
+					
+					thumbnailImg.onload = function() {
+						dojo.fadeIn(thumbnail, 500);
+					}
 				}	
-							
+				
+				// End loading	
 				toggleLoading();
+			},
+			error: function()
+			{
+				cancelRequest('Oops, something went wrong!');
 			}
 		});
 	});
-})
+});
